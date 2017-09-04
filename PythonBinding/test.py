0146b1a752ffde6f422db043374d95ceb50476e4
Alkanoor <juju.schou@gmail.com>
2017-09-04 15:29:53 +0200

    avec un test débile

---
+++ PythonBinding/test.py
@@ -0,0 +1,16 @@
+from AX12 import *
+
+
+a = AX12(129)
+print a.get_status()
+print a.get_position()
+
+b = I2C_bus(9600)
+I2C_bus.init_i2c(115200)
+c = I2C_bus(100000)
+d = I2C_bus(209302)
+d.reset(115200)
+
+print c.baudrate
+print d.baudrate
+print b.baudrate
